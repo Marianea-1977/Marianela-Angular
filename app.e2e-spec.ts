73bd83eb648ada8e7cfa1319a552df940d6fe852
Marianela-1977* <76137113+Marianea-1977@users.noreply.github.com>
2022-04-23 19:11:53 -0600

    Add files via upload

---
+++ app.e2e-spec.ts
@@ -0,0 +1,15 @@
+app.e2e-spec.ts
+import { AppPage } from './app.po';
+
+describe('angular-wishlist-mst App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
